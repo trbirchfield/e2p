2fe6605ed8f111b5f65cfa082279685549cbad07
rjhenriquez <ricardojhenriquez@gmail.com>
2014-01-08 13:59:28 -0600

    After Code Review

--- js/global.js
+++ js/global.js
@@ -1,40 +1,28 @@
 jQuery(document).ready(function($){
-
 	function fade_header() {
-
-		if ( $(window).width() > 300 ) {
-
+		if ($(window).width() > 300) {
 			window_scroll = $(this).scrollTop();
-
-			if ( window_scroll > $('#header').height() ) {
-
+			if (window_scroll > $('#header').height()) {
 				$('#secondary-header').fadeIn();
-
-			} else {
-				
+			} else {				
 				$('#secondary-header').fadeOut('fast');
-
 			}
 		}
-	}
-	
+	}	
 	// Fading welcome message
-
 	function detect_browser_width() {
-		if ( $(window).width() > 800 ) {
+		if ($(window).width() > 800) {
 			$('#header').addClass('fade');
 		} else {
 			$('#header').removeClass('fade');
 		}	
 	}
 	detect_browser_width();
-
 	$(window).resize(function() {
 		detect_browser_width()
 	});
-
 	function fade_welcome() {
-		if ( $(window).width() > 800 ) {
+		if ($(window).width() > 800) {
 
         window_scroll = $(this).scrollTop();
 
@@ -43,10 +31,7 @@ jQuery(document).ready(function($){
 	    	});
 		}
 	}
-
 	$(window).scroll(function() { fade_welcome(); });
-	
-
 	//Scroll to anchor
     $(".scrolltoanchor").click(function() {
         $.scrollTo($($(this).attr("href")), {
@@ -54,24 +39,19 @@ jQuery(document).ready(function($){
         });
         return false;
     });
-
     $(document).ready(function(){
-			$('#selection-door .accordion').click(function(e){
-				e.preventDefault();
-				$(this).closest('li').find('.content').not(':animated').slideToggle();
-				$(this).toggleClass('entry entry-open');
-				$('.final-marker').toggleClass('final-marker final-marker-extended');
-			});
-			$('#selection-door .accordion2').click(function(e){
-				e.preventDefault();
-				$(this).closest('li').find('.content').not(':animated').slideToggle();
-				$(this).toggleClass('entry entry-open');
-				$('.marker-end').toggleClass('marker-end marker-end-extended');
-				$(this).toggleClass('green green-no-margin');
-			});
+		$('#selection-door .accordion').click(function(e){
+			e.preventDefault();
+			$(this).closest('li').find('.content').not(':animated').slideToggle();
+			$(this).toggleClass('entry entry-open');
+			$('.final-marker').toggleClass('final-marker final-marker-extended');
 		});
-	
-
-	
-	
+		$('#selection-door .accordion2').click(function(e){
+			e.preventDefault();
+			$(this).closest('li').find('.content').not(':animated').slideToggle();
+			$(this).toggleClass('entry entry-open');
+			$('.marker-end').toggleClass('marker-end marker-end-extended');
+			$(this).toggleClass('green green-no-margin');
+		});
+	});	
 });
