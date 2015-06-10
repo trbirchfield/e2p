7a963a3d6782828be35c1ccebb4b3fae4c1c0f60
Nolan Morris <nolesm@me.com>
2015-06-10 14:38:10 -0500

    Updating Footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,18 +1,18 @@
 <footer>
 	<div class="footer-img"></div>
 	<div class="container">
-		<h5 class="footer-intro">This site is brought to you by the Community Research Center for Senior Health in Partnership with:</h5>
+		<h5 class="footer-intro">This site is brought to you by the Community Research Center for Senior Health in Collaboration with:</h5>
 		<ul class="footer-logos">
 			<li><img src="images/footer/logo-1.jpg" alt="Central Texas Aging and Disability Resource Center"></li>
-			<li><img src="images/footer/logo-2.jpg" alt="Texas A&#38;M Health Science Center"></li>
-			<li><img src="images/footer/logo-3.jpg" alt="Scott &#38; White Healthcare"></li>
+			<li><img src="images/footer/logo-2.gif" alt="Texas A&#38;M Health Science Center"></li>
+			<li><img src="images/footer/logo-3.gif" alt="Scott &#38; White Healthcare"></li>
 		</ul>
 		<p class="disclaimer">By providing links to other sites, the Community Research Center for Senior Health does not guarantee, approve, or endorse the information or products available on these sites.
 		</p>
 	</div>
 </footer>
 </div>
-<script src="js/global.js"></script> 
+<script src="js/global.js"></script>
 <script src="/js/classie.js"></script>
 <script>
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function()
