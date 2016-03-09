ea2e42f4788786905049d75bf846d09ad699b1b6
nolanm <nolesm@me.com>
2016-03-09 12:12:36 -0600

    About Page Text Updates | Footer Image Update

--- resources/client/views/partials/footer.blade.php
+++ resources/client/views/partials/footer.blade.php
@@ -34,7 +34,7 @@
             <ul class="footer-logos">
                 <li><img src="{{ asset_path('img/footer/logo-3.gif') }}" alt="Scott &#38; White Healthcare"></li>
                 <li><img src="{{ asset_path('img/footer/logo-2.gif') }}" alt="Texas A&#38;M Health Science Center"></li>
-                <li><img src="{{ asset_path('img/footer/logo-1.jpg') }}" alt="Central Texas Aging and Disability Resource Center"></li>
+                <li><img src="{{ asset_path('img/footer/logo-1_2.jpg') }}" alt="Central Texas Aging and Disability Resource Center"></li>
             </ul>
             <p class="disclaimer">
                 By providing links to other sites, the Community Research Center for Senior Health does not guarantee, approve, or endorse the information or products available on these sites.
